978667c224971e97062151b9af31fe0ba67b43ea
yigitozdamar <yigitozdamar@hotmail.com>
2022-03-23 21:51:08 +0300

    redux created

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import millify from "millify";
+import { Row, Col, Statistic, Typography } from "antd";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { Title } = Typography;
+
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total CryptoCurrencies" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Exchanges" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Market Cap" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total 24h Volume" value="5" />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Markets" value="5" />{" "}
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default HomePage;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       tiers: '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+//       'X-RapidAPI-Key': 'f76bd48587mshe0bb491f1ee7d6dp1930c3jsna551213ddafd'
+//     }
+//   };
